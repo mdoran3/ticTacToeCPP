1b7ff0b333a209ea0bfb692dfe4747effa4b165f
mdoran3 <mdoran3@asu.edu>
2023-05-12 19:02:48 -0500

    Add main() method

---
+++ src/ttt.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "************************\n";
+    cout << "Welcome to Tic-Tac-Toe!\n";
+    cout << "************************\n";
+
+    return 0;
+}
